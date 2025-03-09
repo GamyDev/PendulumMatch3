f414f1163f53203a7f292006d3eb13c3acb6ced4
DESKTOP-3G5J0P6\Gamy <Gamydevapp@gmail.com>
2025-03-09 23:44:07 +0300

    Fix CameraAdjuster and UI

--- Assets/Scripts/CameraAdjuster.cs
+++ Assets/Scripts/CameraAdjuster.cs
@@ -2,40 +2,54 @@ using UnityEngine;
 
 public class CameraAdjuster : MonoBehaviour
 {
-    public Camera cam;
+    [SerializeField] private Camera cam;
 
-    private float baseHeight = 1920f;
+    private float baseAspect = 1920f / 1080f; 
+    private float maxAspect = 2520f / 1080f; 
+
     private float minY = 1f, maxY = 3f;
     private float minSize = 5.8f, maxSize = 7.85f;
-    private float maxHeight = 2520f;
+
+    private int lastScreenWidth, lastScreenHeight;
 
     void Start()
     {
         AdjustCamera();
     }
 
+    void Update()
+    {
+        if (Screen.width != lastScreenWidth || Screen.height != lastScreenHeight)
+        {
+            lastScreenWidth = Screen.width;
+            lastScreenHeight = Screen.height;
+            AdjustCamera();
+        }
+    }
+
     void AdjustCamera()
     {
+        Debug.Log("AdjustCamera");
         if (cam == null)
         {
             cam = Camera.main;
         }
 
-        float screenHeight = Screen.height;
+        float currentAspect = (float)Screen.height / Screen.width; 
 
-        if (screenHeight <= baseHeight)
+        if (currentAspect <= baseAspect)
         {
             cam.transform.position = new Vector3(cam.transform.position.x, minY, cam.transform.position.z);
             cam.orthographicSize = minSize;
         }
-        else if (screenHeight >= maxHeight)
+        else if (currentAspect >= maxAspect)
         {
             cam.transform.position = new Vector3(cam.transform.position.x, maxY, cam.transform.position.z);
             cam.orthographicSize = maxSize;
         }
         else
         {
-            float t = (screenHeight - baseHeight) / (maxHeight - baseHeight);
+            float t = (currentAspect - baseAspect) / (maxAspect - baseAspect);
             float newY = Mathf.Lerp(minY, maxY, t);
             float newSize = Mathf.Lerp(minSize, maxSize, t);
 
